5422feeeb9c387f84997b64a48f7749b00dad4df
mohamed ibrahim <103653440+MohamedIbrahim27@users.noreply.github.com>
2023-03-06 15:07:22 +0200

    Initial commi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myproject',
+    version='0.1',
+    description='My Django project',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/MohamedIbrahim27/souqstore.git',
+    packages=find_packages(),
+    install_requires=[
+        'Django==4.1',
+        'requests',
+        # add any other dependencies here
+    ],
+)
