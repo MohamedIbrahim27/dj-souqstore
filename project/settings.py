fcdea3716e3d53d6aea4e3195456324fa6324896
mohamed ibrahim <103653440+MohamedIbrahim27@users.noreply.github.com>
2023-03-09 07:03:56 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(['templates']),
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
